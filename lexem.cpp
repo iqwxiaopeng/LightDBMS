ab07a546330de600a59e50b2c33838a28e6a39c0
Konstantin Besedin <besedin.k@gmail.com>
2015-11-20 12:42:52 +0500

    Мелкие правки.

--- lexem.cpp
+++ lexem.cpp
@@ -1,7 +1,7 @@
 #include "lexem.h"
 #include <string>
 
-Lexem::Lexem(LexemType type, std::string value) : type(type), value(value) {};
+Lexem::Lexem(LexemType type, std::string value) : type(type), value(value) {}
 LexemType Lexem::getType() {
 	return type;
 }
